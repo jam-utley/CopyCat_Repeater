d7e15bb03b076ed7cfb7eee89c57dfeac1673495
James Utley <james.utley000@gmail.com>
2025-01-13 13:49:06 -0500

    Fixed the rewriting problem
    But the recording sessions still aren't recording the data properly. quotation marks keep getting added. Could it be due to errant print statements?

--- CopyCat_v1_binary.py
+++ CopyCat_v1_binary.py
@@ -13,40 +13,39 @@ def copycat_binary_record():
     num_in = int(input("Enter a 0 or a 1. Press 2 to stop."))
     num_in_list = []
     again = 0       #Variable that when above zeero will stop the encoding process.
+    f = open('cc_binary_record.txt', 'r+')
     while again <1:
         num_in_list = list(num_in_list)
         while num_in > 2 or num_in<0:     #Error checking
             num_in = int(input("Please enter a valid selection."))
         while num_in < 2:
+            num_in_list.clear()
             num_in_list.append(num_in)       #Appends the input into a list
             num_in = int(input("Enter a 0 or a 1. Press 2 to stop."))   #Gets a new input
         if first_time_counter > 0:
-            print("eek")
-            with open("cc_binary_record.txt", 'r') as f:
-                for line in f:  #Reads the last line and stores it in a variable for slicing
-                    pass
-                lastline = line
-                (left,sep,right) = lastline.partition(':')   #partitions the string to find the version number
-                session_num = left[-1]             #Finds the previous session number and records it
-                session_num = int(session_num)
-            with open("cc_binary_record.txt", 'w') as f:
-                num_in_list = str(num_in_list)
-                session_num += 1
-                f.write("Session v")   #Records session version
-                session_num = str(session_num)
-                f.write(session_num)   #Version number
-                f.write(":")           #Adds a colon afte the version number
-                f.write(num_in_list)   #Writes the binary list to the file
-        else:
-            with open("cc_binary_record.txt", 'w') as f:  #For the first time through the file
-                num_in_list = str(num_in_list)
-                f.write("Session v1: ")   #Records session version
-                f.write(num_in_list)   #Writes the binary list to the file
-                f.write('\n')
+            first_time_counter +=1
+            print("eek")            
+            for line in f:  #Reads the last line and stores it in a variable for slicing
+                pass
+            num_in_list = str(num_in_list)
+            f.write("Session v")   #Records session version
+            first_time_counter = str(first_time_counter)
+            f.write(first_time_counter)   #Version number
+            first_time_counter = int(first_time_counter)
+            f.write(":")           #Adds a colon afte the version number
+            f.write(num_in_list)   #Writes the binary list to the file
+            f.write('\n')
+        else:   #If it's the first time, it records:
+           
+            num_in_list = str(num_in_list)
+            f.write("Session v1: ")   #Records session version
+            f.write(num_in_list)   #Writes the binary list to the file
+            f.write('\n')
             first_time_counter += 1
         endit = input("Do you wish to encode another session? Y/N ")
         if endit.lower() == 'n':    #Adds an exit criterium for the loop
             again = 1  
+            f.close()
         else:
             num_in = int(input("Enter a 0 or a 1. Press 2 to stop."))
     print("Encoding complete.")
