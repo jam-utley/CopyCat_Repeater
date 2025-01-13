547a5663a49f88ec71561aa2fc19919818434579
James Utley <james.utley000@gmail.com>
2025-01-13 15:38:29 -0500

    Added functions
    New function for reviewing session data on command ; added session version numbers to the text file

--- CopyCat_v1.2_binary.py
+++ CopyCat_v1.2_binary.py
@@ -9,6 +9,8 @@ def copycat_binary_record():
     """
     Takes input of either a zero or a one, writes it to a dedicated file
     """
+    with open('cc_binary_record.txt', 'w') as f:    #Opens and closes the file, clearing it.
+        pass
     first_time_counter = 0
     num_in = int(input("Enter a 0 or a 1. Press 2 to stop."))
     num_in_list = []
@@ -25,27 +27,34 @@ def copycat_binary_record():
         if first_time_counter > 0:
             first_time_counter +=1
             num_in_list = str(num_in_list)
-            f.write(num_in_list)   #Writes the binary list to the file
+            f.write(f"Session {first_time_counter}: {num_in_list}")   #Writes the binary list to the file
             f.write('\n')
         else:   #If it's the first time, it records:
            
             num_in_list = str(num_in_list)
-            f.write(num_in_list)   #Writes the binary list to the file
+            f.write(f"Session 1: {num_in_list}")   #Writes the binary list to the file
             f.write('\n')
             first_time_counter += 1
-        endit = input("Do you wish to encode another session? Y/N ")
+        endit = input("Do you wish to record another session? Y/N ")
         if endit.lower() == 'n':    #Adds an exit criterium for the loop
             playagain = 1  
             f.close()
         else:
             num_in = int(input("Enter a 0 or a 1. Press 2 to stop."))
-    print("Encoding complete.")
+    print("Recording complete.")
 
-def copycat_binary_read(ver_num):
+def copycat_binary_read(session_num):
     """
     Reads the binary file at a specified line number corresponding to the session recorded.
     """
-
+    f = open('cc_binary_record.txt', 'r')
+    for i in range(session_num):
+        line = f.readline()
+    if line == '':
+        print("No recorded session.")
+    else:
+        print(line)
     return(None)
 
-copycat_binary_record()
+#copycat_binary_record()
+copycat_binary_read(int(input("which number session do you want to see?")))
